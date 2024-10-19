5763e579a5dc94fbfd213efeca25f058b8790c2d
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-19 17:17:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('why are y
-           ou ✅️✅️gay?')
+  res.send('why are you gay?✅️✅️')
 })
 
 app.listen(3000)
