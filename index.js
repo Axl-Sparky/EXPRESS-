9943e875f09bd494094053680a264a9ce09f1be6
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-20 20:50:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const cors = require('cors');
 const express = require('express');
-const path = require ('path');
-const { fileURLToPath }  = require('url');
-//const xnxxdl = require('./ax/xnxxdl.js');
+const path = require('path');
+const { fileURLToPath } = require('url');
+const xnxxdl = require('./ax/xnxxdl.js');
 
 const app = express()
 app.set('json spaces', 2)
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     creator: 'Ameen Int'
   })})
   
-//app.use('/xnx', xnxxdl) // ninak vende name /xnx mattiya madhi
+app.use('/xnx', xnxxdl) // ninak vende name /xnx mattiya madhi
 
 app.listen(8080, () => {
   console.log('api by ameen. running on 8080')
