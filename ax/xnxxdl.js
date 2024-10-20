ae63be1cf7e615cc89437cff50326db288d387b4
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-20 11:05:04 +0530

    Create xnxxdl.js

---
+++ ax/xnxxdl.js
@@ -0,0 +1,13 @@
+import xnxxDownloader from '../func/xnxx.js'
+import express from 'express'
+
+var router = express.Router()
+
+router.get('/', async (req, res) => {
+  var query = req.query.url
+  if (!query) return res.json({ creator: 'Myr', status: false, msg: 'url is required' })
+  var xdownload = await xnxxDownloader(query)
+  res.json(xdownload)
+})
+
+export default router
