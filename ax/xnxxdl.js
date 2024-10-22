0350665cec22c16c2f15d09a4c3c5ddd5941a39b
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-22 08:04:42 +0530

    Update xnxxdl.js

--- ax/xnxxdl.js
+++ ax/xnxxdl.js
@@ -4,7 +4,7 @@ var router = express.Router()
 
 router.get('/', async (req, res) => {
   var query = req.query.url
-  if (!query) return res.json({ creator: 'Myr', status: false, msg: 'url is required' })
+  if (!query) return res.json({ creator: 'AXL-SPARKY', status: false, msg: 'url is required' })
   var xdownload = await xnxxDownloader(query)
   res.json(xdownload)
 })
