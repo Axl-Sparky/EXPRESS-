c0bd7b58b7be0b21f66d4571e6e64306dcc22547
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-18 14:27:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const cors = require('cors');
 const express = require('express');
-//const path = require('path');
-
-//const { fileURLToPath } = require('url');
-//const xnxxdl = require('./ax/xnxxdl.js');
-//const __filename = fileURLToPath(import.meta.url)
-//const __dirname = path.dirname(__filename)
 const app = express()
 app.set('json spaces', 2)
 
@@ -14,7 +8,6 @@ app.get('/', (req, res) => {
     creator: 'Unknown One'
   })})
   
-//app.use('/xnx', xnxxdl) // ninak vende name /xnx mattiya madhi
 
 app.listen(8080, () => {
   console.log('running')
