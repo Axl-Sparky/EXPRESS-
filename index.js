bb39e40f699833e4d2a41eb82236bc8afe491118
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-21 17:58:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ import express from 'express';
 import('node-fetch').then(fetch => { // For making HTTP requests
 const app = express();
 
+const axios = require('axios')
+
+
+const getAxl = async function(url, options) {
+  try {
+    options ? options : {};
+    const res = await axios({
+      method: "GET",
+      url: url,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+      },
+      ...options,
+    });
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+}
+
+
+
 // Set JSON formatting for response
 app.set('json spaces', 2);
 
@@ -19,7 +42,7 @@ app.get('/sfys', async (req, res) => {
     
     // Make a request to the external API with the provided query
     const apiUrl = `https://ameen-api.vercel.app/sfys?query=${query}`;
-    const response = await fetch(apiUrl);
+    const response = await getAxl(apiUrl);
   
     // If the response is not OK, return an error
     if (!response.ok) {
@@ -27,7 +50,7 @@ app.get('/sfys', async (req, res) => {
     }
 
     // Parse the JSON response from the external API
-    const rsi = await response.json();
+    const rsi = await response.data
 
     // Assuming the response from the external API contains a 'download_url' field
     if (!rsi) {
