5f8e630b44be530e96c2bb34a0822524a1b89fe0
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-21 18:32:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,6 @@ import express from 'express';
 
 const app = express();
 
-import axios from 'axios';
-
-
-
-const getAxl = async function(url, options) {
-  try {
-    options ? options : {};
-    const res = await axios({
-      method: "GET",
-      url: url,
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
-      },
-      ...options,
-    });
-    return res.data;
-  } catch (err) {
-    return err;
-  }
-}
-
-
 
 // Set JSON formatting for response
 app.set('json spaces', 2);
