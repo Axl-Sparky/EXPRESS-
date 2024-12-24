1179236edf79c563cba68dcadb08ac306e2caa03
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-24 11:44:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 const app = express()
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
 
 
@@ -10,11 +12,19 @@ app.set('json spaces', 2)
 
 app.get('/sfys', (req, res) => {
 const query = req.query.query
+
+const search = await yts(query)
+const data = search.videos[0];
+const yturl = data.url
+
+
+let down = await fg.yta(yturl)
+let downloadUrl = down.dl_url
   
   res.json({
     creator: 'Unknown One',
     track : query,
-    data : 'endi'
+    url : downloadUrl
   })})
 
   
