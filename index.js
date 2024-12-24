e99ab9af57b456adcb0d1340ce4276928292ba30
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-24 11:32:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,23 @@
 const cors = require('cors');
 const express = require('express');
 const app = express()
-//const { getAxl }= ('../functions.js');
 
 
-const fetch = require('node-fetch')
 
 
-/*const gethAxl = async function(url, options) {
-  try {
-    options ? options : {};
-    const res = await axios({
-      method: "GET",
-      url: url,
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
-      },
-      ...options,
-    });
-    return res.data;
-  } catch (err) {
-    return err;
-  }
-}
-*/
+
 app.set('json spaces', 2)
 
 app.get('/sfys', (req, res) => {
 
-
-const api = ("https://ameen-api.vercel.app/sfys?query=")
- 
-  
-const query = req.query.query;  // This is the query parameter
-const url = (api + query)
-
-  const response =  fetch(url)
- // const resi = response.data
-  //const resdata 
   const resp = response.json()
   
   res.json({
     creator: 'Unknown One',
     track : query,
-    data : resp.data
+    data : 'endi'
   })})
 
-
-  
-
-  
   
 
 app.listen(8080, () => {
