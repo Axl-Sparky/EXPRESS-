8c64b4e1b89412e73c8ccf28a4ba3c9748e5f9d7
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2025-05-08 22:47:07 +0530

    Delete api/data.js

--- api/data.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-export default function handler(req, res) {
-  const json = fs.readFileSync('data.json', 'utf-8');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(json);
-}
