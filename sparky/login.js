9e60621910957fd6e232f9b5fef586789bc1e007
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2025-05-09 12:07:40 +0530

    Delete sparky directory

--- sparky/login.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  const password = document.getElementById("password").value;
-  if (password === "₹ajsal120&") {
-    localStorage.setItem("auth", "yes");
-    window.location.href = "./dashboard.html";
-  } else {
-    document.getElementById("error").innerText = "Incorrect password.";
-  }
-}
